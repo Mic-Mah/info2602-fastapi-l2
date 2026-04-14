e12b6402d22dbd50ffa2245752fcd899cb9a51ad
Micah Mahabir <mahabirm05915@gmail.com>
2026-04-14 12:57:39 -0400

    Enhance User model with constructor and password hashing
    Added constructor and password hashing method to User model.

--- app/models.py
+++ app/models.py
@@ -9,3 +9,14 @@ class User(SQLModel, table=True):
     username:str = Field(index=True, unique=True)
     email:str = Field(index=True, unique=True)
     password:str
+
+    def __init__(self, username:str, email:str, password:str):
+        self.username = username
+        self.email = email
+        self.set_password(password)
+
+    def set_password(self, password:str):
+        self.password = password_hash.hash(password)
+
+    def __str__(self) -> str:
+        return f"User id={self.id}, username={self.username}, email={self.email}"
